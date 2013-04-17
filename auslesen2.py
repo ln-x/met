6e50573c49d7ae86c367238dd7f3cca05d948d3b
wasserblum <wasserblum@gmx.at>
2013-04-17 11:31:23 +0200

    reformatted

--- auslesen2.py
+++ auslesen2.py
@@ -9,7 +9,7 @@ x = []
 
 csv_reader = csv.reader(open('/home/lnx/Documents/_BioCLIC/_Messungen/inklMittlereTagesgaenge_Lafnitz20130401.csv'))
 
-graphiken ={}
+graphiken = {}
 
 for line in csv_reader:
     #print line
@@ -18,8 +18,8 @@ for line in csv_reader:
     if any(c.isalpha() for c in splitted[0]):
         #print splitted
         name = splitted[0]
-        listezeit = [dt.datetime.strptime(x,"%H:%M") for x in splitted[1:23]]
-        graphiken[name] = {'time':listezeit}
+        listezeit = [dt.datetime.strptime(x, "%H:%M") for x in splitted[1:23]]
+        graphiken[name] = {'time': listezeit}
         #print name , listezeit
     else:
         monat = int(float(splitted[0]))
@@ -29,7 +29,7 @@ for line in csv_reader:
         graphiken[name][monat] = tagesdaten
         #print'datenzeile'
 
-    #print name, listezeit
+        #print name, listezeit
 
 #print graphiken
 #print graphiken.keys()
@@ -45,9 +45,9 @@ ax.plot(graphiken['RO'][9], color='darkred', lw=0.5)
 ax.plot(graphiken['RO'][10], color='violet', lw=0.5)
 ax.plot(graphiken['RO'][11], color='darkblue', lw=0.5)
 #ax.plot(graphiken['RO'][12], color='blue', lw=0.5)
-plt.axis([1,23,5,20])
-plt.setp(plt.gca(), xticklabels=[6,12,18], yticks=(10,15,20), xticks=(6,12,18))
-plt.text(20,18, 'I', fontsize=20)
+plt.axis([1, 23, 5, 20])
+plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+plt.text(20, 18, 'I', fontsize=20)
 plt.ylabel('water temperature')
 
 
@@ -59,9 +59,9 @@ ax.plot(graphiken['NS WT12'][8], color='red', lw=0.5)
 ax.plot(graphiken['NS WT12'][9], color='darkred', lw=0.5)
 ax.plot(graphiken['NS WT12'][10], color='violet', lw=0.5)
 #ax.plot(graphiken['NS WT12'][11], color='darkblue', lw=0.5)
-plt.text(20,18, 'II', fontsize=20)
-plt.setp(plt.gca(), xticklabels=[6,12,18], yticks=(10,15,20), xticks=(6,12,18))
-plt.axis([1,23,5,20])
+plt.text(20, 18, 'II', fontsize=20)
+plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+plt.axis([1, 23, 5, 20])
 
 ax = fig.add_subplot(223)
 ax.plot(graphiken['ND WT13'][7], color='orange', lw=0.5)
@@ -69,9 +69,9 @@ ax.plot(graphiken['ND WT13'][8], color='red', lw=0.5)
 ax.plot(graphiken['ND WT13'][9], color='darkred', lw=0.5)
 ax.plot(graphiken['ND WT13'][10], color='violet', lw=0.5)
 ax.plot(graphiken['ND WT13'][11], color='darkblue', lw=0.5)
-plt.axis([1,23,5,20])
-plt.setp(plt.gca(), xticklabels=[6,12,18], yticks=(10,15,20), xticks=(6,12,18))
-plt.text(20,18, 'III', fontsize=20)
+plt.axis([1, 23, 5, 20])
+plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+plt.text(20, 18, 'III', fontsize=20)
 plt.ylabel('water temperature')
 plt.xlabel('hours')
 
@@ -81,9 +81,9 @@ ax.plot(graphiken['DK WT14'][8], color='red', lw=0.5)
 ax.plot(graphiken['DK WT14'][9], color='darkred', lw=0.5)
 ax.plot(graphiken['DK WT14'][10], color='violet', lw=0.5)
 ax.plot(graphiken['DK WT14'][11], color='darkblue', lw=0.5)
-plt.axis([1,23,5,20])
-plt.setp(plt.gca(), xticklabels=[6,12,18], yticks=(10,15,20), xticks=(6,12,18))
-plt.text(20,18, 'IV', fontsize=20)
+plt.axis([1, 23, 5, 20])
+plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+plt.text(20, 18, 'IV', fontsize=20)
 plt.xlabel('hours')
 
 
