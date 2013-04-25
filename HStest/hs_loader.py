7c28c8716bc2bfa15947304858cc503f42790552
wasserblum <wasserblum@gmx.at>
2013-04-25 19:27:07 +0200

    hs_loader daten eingelesen in liste

--- HStest/hs_loader.py
+++ HStest/hs_loader.py
@@ -2,38 +2,41 @@ from __future__ import division
 __author__ = 'lnx'
 import datetime
 
-file = "/home/lnx/PycharmProjects/Messdatenauswertung/HStest/v808_20130411_c00_v01_f03/Heat_Cond.txt"
 
-with open(file,"r") as f:    #Einlesen des Files in eine Liste
-    alldata = f.readlines()
-    #print len(alldata)
-    #print type(alldata)
+def loadfile(filename):
+    with open(filename,"r") as f:    #Einlesen des Files in eine Liste
+        alldata = f.readlines()
+        #print len(alldata)
+        #print type(alldata)
 
-data = alldata[3:]  #Liste ab 4.Zeile der alten Liste - Beginn der Rohdaten
+    data = alldata[3:]  #Liste ab 4.Zeile der alten Liste - Beginn der Rohdaten
 
-splitdata = []       #splitlistcomp = [i.split() for i in data]
-for i in data:
-    splitdata.append(i.split())  #Splitten der Listenelemente   split(":")  Seperator ":"
+    splitdata = []       #splitlistcomp = [i.split() for i in data]
+    for i in data:
+        splitdata.append(i.split())  #Splitten der Listenelemente   split(":")  Seperator ":"
 
-converteddata = []
-begin = datetime.datetime(1900,1,1,0,0,0) #Startdatum
-for j in splitdata:
-    tage,frac = j[0].split('.')
-    zeit = round((float(frac)*24)/1000000)   # 500000 = 12h, 250000 = 6h, 041667 = 1h
-    date_time = begin + datetime.timedelta(days=float(tage)) + datetime.timedelta(hours=zeit)
-    x1 = float(j[1])
-    x2 = float(j[2])
-    x3 = float(j[3])
-    x4 = float(j[4])
+    converteddata = []
+    begin = datetime.datetime(1900,1,1,0,0,0) #Startdatum
+    for j in splitdata:
+        tage,frac = j[0].split('.')
+        zeit = round((float(frac)*24)/1000000)   # 500000 = 12h, 250000 = 6h, 041667 = 1h
+        date_time = begin + datetime.timedelta(days=float(tage)) + datetime.timedelta(hours=zeit)
+        x1 = float(j[1])
+        x2 = float(j[2])
+        x3 = float(j[3])
+        x4 = float(j[4])
+        converteddata.append([date_time,x1,x2,x3,x4])
 
-    converteddata.append([date_time,x1,x2,x3,x4])
+#    for d in converteddata:
+#        print d
 
-for d in converteddata:
-    print d
-
-
+    return converteddata
 
 
+if __name__ == '__main__':
+    filename = "/home/lnx/PycharmProjects/Messdatenauswertung/HStest/v808_20130411_c00_v01_f03/Heat_Cond.txt"
+    thedata = loadfile(filename=filename)
+    print 'loaded: ', thedata
 
 
 
---
+++ HStest/hs_plot.py
@@ -0,0 +1,63 @@
+__author__ = 'lnx'
+import hs_loader
+import matplotlib.pyplot as plt
+
+filename = "/home/lnx/PycharmProjects/Messdatenauswertung/HStest/v808_20130411_c00_v01_f03/Heat_Cond.txt"
+thedata = hs_loader.loadfile(filename=filename)
+print 'loaded: ', thedata
+
+date_time = [i[0] for i in thedata]
+x1 = [i[1] for i in thedata]
+x2 = [i[2] for i in thedata]
+x3 = [i[3] for i in thedata]
+x4 = [i[4] for i in thedata]
+
+fig = plt.figure()
+
+ax = fig.add_subplot(221)
+ax.plot(date_time, x1, color='red', lw=0.5)
+ax.plot(date_time, x2, color='darkred', lw=0.5)
+ax.plot(date_time, x3, color='violet', lw=0.5)
+ax.plot(date_time, x4, color='darkblue', lw=0.5)
+#ax.plot(graphiken['RO'][12], color='blue', lw=0.5)
+#plt.axis([0, 15, -15, 15])
+#plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+#plt.text(20, 18, 'I', fontsize=20)
+#plt.ylabel('water temperature')
+#plt.ylim(-15,15)
+
+# ax = fig.add_subplot(222)
+# ax.plot(graphiken['NS WT12'][7], color='orange', lw=0.5)
+# ax.plot(graphiken['NS WT12'][8], color='red', lw=0.5)
+# ax.plot(graphiken['NS WT12'][9], color='darkred', lw=0.5)
+# ax.plot(graphiken['NS WT12'][10], color='violet', lw=0.5)
+# #ax.plot(graphiken['NS WT12'][11], color='darkblue', lw=0.5)
+# plt.text(20, 18, 'II', fontsize=20)
+# plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+# plt.axis([1, 23, 5, 20])
+#
+# ax = fig.add_subplot(223)
+# ax.plot(graphiken['ND WT13'][7], color='orange', lw=0.5)
+# ax.plot(graphiken['ND WT13'][8], color='red', lw=0.5)
+# ax.plot(graphiken['ND WT13'][9], color='darkred', lw=0.5)
+# ax.plot(graphiken['ND WT13'][10], color='violet', lw=0.5)
+# ax.plot(graphiken['ND WT13'][11], color='darkblue', lw=0.5)
+# plt.axis([1, 23, 5, 20])
+# plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+# plt.text(20, 18, 'III', fontsize=20)
+# plt.ylabel('water temperature')
+# plt.xlabel('hours')
+#
+# ax = fig.add_subplot(224)
+# ax.plot(graphiken['DK WT14'][7], color='orange', lw=0.5)
+# ax.plot(graphiken['DK WT14'][8], color='red', lw=0.5)
+# ax.plot(graphiken['DK WT14'][9], color='darkred', lw=0.5)
+# ax.plot(graphiken['DK WT14'][10], color='violet', lw=0.5)
+# ax.plot(graphiken['DK WT14'][11], color='darkblue', lw=0.5)
+# plt.axis([1, 23, 5, 20])
+# plt.setp(plt.gca(), xticklabels=[6, 12, 18], yticks=(10, 15, 20), xticks=(6, 12, 18))
+# plt.text(20, 18, 'IV', fontsize=20)
+# plt.xlabel('hours')
+
+# fig.savefig('albedo.png')
+plt.show()
