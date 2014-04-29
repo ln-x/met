451eb6bf19877fb9b7521bde1e2c85bcc0c7c607
wasserblum <wasserblum@gmx.at>
2014-04-29 16:55:34 +0200

    mm

--- Hs_scripts/hs_plot_6h_fkm.py
+++ Hs_scripts/hs_plot_6h_fkm.py
@@ -11,16 +11,6 @@ thedata = hs9_loader.loadfile(filename=filename)
 date_time = [i[0] for i in thedata]
 thedata_withouttime = [i[1:] for i in thedata]
 
-c = len(thedata_withouttime)
-print 'thedata_withouttime:\n', thedata_withouttime[3]
-data_transposed = [[] for i in range (c)]
-for x in thedata_withouttime:
-    for y, z in zip(data_transposed, x):
-        y.append(z)
-print 'data_transposed:\n', data_transposed[171]
-
-Tmin = []
-Tmax = []
 Rkm =  [93.0,92.5,92.0,91.5,91.0,90.5,90.0,89.5,89.0,88.5,88.0,87.5,87.0,86.5,86.0,85.5,85.0,84.5,84.0,
         83.5,83.0,82.5,82.0,81.5,81.0,80.5,80.0,79.5,79.0,78.5,78.0,77.5,77.0,76.5,76.0,75.5,75.0,74.5,74.0,73.5,73.0,
         72.5,72.0,71.5,71.0,70.5,70.0,69.5,69.0,68.5,68.0,67.5,67.0,66.5,66.0,65.5,65.0,64.5,64.0,63.5,63.0,62.5,62.0,
@@ -30,26 +20,35 @@ Rkm =  [93.0,92.5,92.0,91.5,91.0,90.5,90.0,89.5,89.0,88.5,88.0,87.5,87.0,86.5,86
         28.5,28.0,27.5,27.0,26.5,26.0,25.5,25.0,24.5,24.0,23.5,23.0,22.5,22.0,21.5,21.0,20.5,20.0,19.5,19.0,18.5,18.0,
         17.5,17.0,16.5,16.0,15.5,15.0,14.5,14.0,13.5,13.0,12.5,12.0,11.5,11.0,10.5,10.0,9.5,9.0,8.5,8.0,7.5]
 
-for y in data_transposed:
-    Tmin0, idx = min((Tmin0, idx) for (idx,Tmin0) in enumerate (y))
-    Tmin.append(Tmin0)
-    print min((Tmin0, idx) for (idx,Tmin0) in enumerate (y))
-    print len(Tmin)
-    print type(Tmin0)
-    Tmax0, idx = max((Tmax0, idx) for (idx, Tmax0) in enumerate (y))
-    Tmax.append(Tmax0)
+# T6h = thedata_withouttime[6] #1.Juli 2013 minimum of km93
+# T14h = thedata_withouttime[14] #1.Juli 2013 maximum of km7.5
+# T17h = thedata_withouttime[17] #1.Juli 2013 maximum of km93
+# T25h = thedata_withouttime[25] #2.Juli 2013 minimum of km7.5
 
-print Tmin[171]
-#print Tmax
-#print len(Rkm), len(Tmin), len(Tmax)
+T6h = thedata_withouttime[6]
+T9h = thedata_withouttime[9]
+T12h = thedata_withouttime[12]
+T15h = thedata_withouttime[15]
+T18h = thedata_withouttime[18]
+T21h = thedata_withouttime[21]
+T24h = thedata_withouttime[24]
+T27h = thedata_withouttime[27]
 
 fig = plt.figure()
 
-plt.title('Tmin, Tmax')
-plt.plot(date_time, Tmin, color='blue', lw=0.5)
-#plt.plot(Rkm, Tmax, color='red', lw =0.5)
+plt.title('1.July 2013 6am - 2.July 3am')
+plt.plot(Rkm, T6h, color='blue', lw=0.5, label='6h')
+plt.plot(Rkm, T9h, color='lightblue', lw=0.5, label='9h')
+plt.plot(Rkm, T12h, color='turquoise', lw =0.5,label='12h')
+plt.plot(Rkm, T15h, color='lightgreen', lw =0.5, label='15h')
+plt.plot(Rkm, T18h, color='green', lw =0.5, label='18h')
+plt.plot(Rkm, T21h, color='yellow', lw =0.5, label='21h')
+plt.plot(Rkm, T24h, color='orange', lw =0.5, label='24h')
+plt.plot(Rkm, T27h, color='red', lw =0.5, label='27h')
 
 plt.ylabel('water temperature[degC]')
-plt.xlabel('river distance from mouth?[km]')
+plt.xlabel('river distance from mouth [km]')
+
+plt.legend(bbox_to_anchor=(1.00, 1), loc=2, borderaxespad=0.)
 
 plt.show()
--- Hs_scripts/hs_plot_tagesgaenge_standorte.py
+++ Hs_scripts/hs_plot_tagesgaenge_standorte.py
@@ -5,15 +5,15 @@ from Hs_scripts import hs_cdataloader
 __author__ = 'lnx'
 import matplotlib.pyplot as plt
 
-filename = "C:\heatsource900b5_339\MessdatenPinkaGSbisBG_20130702_09.txt"
+filename = "C:\heatsource900b5_339\MessdatenPinkaGSbisMO_20130701_0829.txt"
 mdata = hs9_loader.loadfile(filename=filename)
-print 'loaded: ', mdata
+#print 'loaded: ', mdata
 
 date_time = [i[0] for i in mdata]
-#x4 = [i[1] for i in thedata] #Messdaten Glaserstrasse
+#m0 = [i[1] for i in thedata] #Messdaten Glaserstrasse
 m1 = [i[3] for i in mdata] #Messdaten Hundsmühlbach
 m2 = [i[5] for i in mdata] #Messdaten Tauchenbach
-m3 = [i[7] for i in mdata] #Messdaten Sinnersdorf
+m3 = [i[8] for i in mdata] #Messdaten Sinnersdorf
 m4 = [i[8] for i in mdata] #Messdaten Riedlingsdorf
 m5 = [i[9] for i in mdata] #Messdaten ober Oberwart
 m6 = [i[10] for i in mdata] #Messdaten unter Oberwart
@@ -26,52 +26,75 @@ m12 = [i[17] for i in mdata] #Messdaten Zickenbach2
 m13 = [i[18] for i in mdata] #Messdaten Badersdorf
 m14 = [i[19] for i in mdata] #Messdaten Woppendorf
 m15 = [i[20] for i in mdata] #Messdaten Burg
+m16 = [i[30] for i in mdata] #Messdaten Burg LF
+m17 = [i[31] for i in mdata] #Messdaten Eisenberg
+m18 = [i[32] for i in mdata] #Messdaten Deutsch-Kaltenbrunn Restwasser
+m19 = [i[33] for i in mdata] #Messdaten Deutsch-Kaltenbrunn Pegel
+m20 = [i[34] for i in mdata] #Messdaten Deutsch-Kaltenbrunn Unterwasser
+m21 = [i[35] for i in mdata] #Messdaten Bildein Pegel
+m22 = [i[36] for i in mdata] #Messdaten Bildein Unterwasser
+m23 = [i[38] for i in mdata] #Messdaten Bildein Restwasser
+m24 = [i[39] for i in mdata] #Messdaten Eberau
+m25 = [i[40] for i in mdata] #Messdaten Postrum
+m26 = [i[41] for i in mdata] #Messdaten Gaas - Rodlingbach
+m27 = [i[42] for i in mdata] #Messdaten Gaas
+#m28 = [i[43] for i in mdata] #Messdaten Moschendorf
 
 filename = "C:\heatsource900b5_339\win32\Test\outputfiles\Temp_H2O.txt"
 thedata = hs9_loader.loadfile(filename=filename)
-print 'loaded: ', thedata
+#print 'loaded: ', thedata
+
+#print i[43] TODO: Fehler: m28 = [i[43] for ...] list index out of range obwohl print i[43] funkt, kein sichtbares Prob.
 
 date_time = [i[0] for i in thedata]
-x1 = [i[10] for i in thedata] #km 88.5 Hundsmühlbach
-x2 = [i[18] for i in thedata] #km 84.5 Tauchenbach
-x3 = [i[28] for i in thedata] #km 79.5 Sinnersdorf
-x4 = [i[49] for i in thedata] #km 69 Riedlingsdorf
-#x5 = [i[58] for i in thedata] #km 64.5 ober Oberwart
-#x6 = [i[64] for i in thedata] #km 61.5 unter Oberwart
-x7 = [i[66] for i in thedata] #km 60.5 Unterwart
-x8 = [i[76] for i in thedata] #km 55.5 Jabbing 1
-x9 = [i[79] for i in thedata] #km 54 Jabbing 2+3
-x10 = [i[85] for i in thedata] #km 51 Jabbing 4
-x11 = [i[94] for i in thedata] #km 46.5 Zickenbach 1+2
-x12 = [i[98] for i in thedata] #km 44.5 Badersdorf
-x13 = [i[106] for i in thedata] #km 40.5 Woppendorf
-x14 = [i[113] for i in thedata] #km 37 Burg (BioClic)
-x15 = [i[130] for i in thedata] #km 28.5 DeutschSchützen
-x16 = [i[140] for i in thedata] #km 23.5 Bildein Unterwasser
-x17 = [i[144] for i in thedata] #km 21.5 Eberau
-x18 = [i[154] for i in thedata] #km 16.5 Moschendorf
+x1 = [i[8] for i in thedata] #km 88.92 Hundsmühlbach
+x2 = [i[17] for i in thedata] #km 84.60 Tauchenbach
+x3 = [i[26] for i in thedata] #km 80.10 Sinnersdorf
+x4 = [i[48] for i in thedata] #km 69.16 Riedlingsdorf
+x5 = [i[56] for i in thedata] #km 64.98 ober Oberwart
+x6 = [i[61] for i in thedata] #km 62.62 unter Oberwart
+x7 = [i[64] for i in thedata] #km 60.83 Unterwart
+x8 = [i[75] for i in thedata] #km 55.56 Jabbing 1
+x9 = [i[76] for i in thedata] #km 54.42 Jabbing 2
+x10 = [i[77] for i in thedata] #km 54.26 Jabbing 3
+x11 = [i[83] for i in thedata] #km 51.41 Jabbing 4
+x12 = [i[92] for i in thedata] #km 46.70 Zickenbach 1
+x13 = [i[93] for i in thedata] #km 46.51 Zickenbach 2
+x14 = [i[96] for i in thedata] #km 44.65 Badersdorf
+x15 = [i[104] for i in thedata] #km 40.96 Woppendorf
+x16 = [i[110] for i in thedata] #km 38.19 Burg (BioClic)
+x17 = [i[111] for i in thedata] #km 37.29 Burg (LowFlow)
+x18 = [i[124] for i in thedata] #km 30.96 Eisenberg
+x19 = [i[128] for i in thedata] #km 28.79 DeutschSchützen R
+x20 = [i[129] for i in thedata] #km 28.44/28.38 DeutschSchützen P+U
+x21 = [i[135] for i in thedata] #km 25.32 Bildein Pegel
+x22 = [i[138] for i in thedata] #km 23.96/23.78 Bildein Unterwasser+R
+x23 = [i[144] for i in thedata] #km 21.0 Eberau
+x25 = [i[146] for i in thedata] #km 20.14 Postrum
+x26 = [i[152] for i in thedata] #km 17.0 Gaas
+x27 = [i[157] for i in thedata] #km 14.61 Moschendorf
 
 fig = plt.figure()
 
 ax = fig.add_subplot(221)
 ax.set_title('HB, TB, SD, RD')
-ax.plot(date_time, x1, color='red', lw=0.2)      #HB
-ax.plot(date_time, x2, color='darkred', lw=0.2)  #TB
-ax.plot(date_time, x3, color='violet', lw=0.2)   #SD
-ax.plot(date_time, x4, color='darkblue', lw=0.2) #RD
-ax.plot(date_time, m1, color='red', lw=0.7)
-ax.plot(date_time, m2, color='darkred', lw=0.7)
-ax.plot(date_time, m3, color='violet', lw=0.7)
-ax.plot(date_time, m4, color='darkblue', lw=0.7)
+ax.plot(date_time, x1, color='red', lw=0.2)      #S HB
+ax.plot(date_time, x2, color='darkred', lw=0.2)  #S TB
+ax.plot(date_time, x3, color='violet', lw=0.2)   #S SD
+ax.plot(date_time, x4, color='darkblue', lw=0.2) #S RD
+ax.plot(date_time, m1, color='red', lw=0.7)      #M HB
+ax.plot(date_time, m2, color='darkred', lw=0.7)  #M TB
+ax.plot(date_time, m3, color='violet', lw=0.7)   #M SD
+ax.plot(date_time, m4, color='darkblue', lw=0.7) #M RD
 plt.ylabel('water temperature[degC]')
 fig.autofmt_xdate()
 
 ax = fig.add_subplot(222)
-ax.set_title('UW, J1, J2+3, J4')
-ax.plot(date_time, x7, color='red', lw=0.2)
-ax.plot(date_time, x8, color='darkred', lw=0.2)
-ax.plot(date_time, x9, color='darkviolet', lw=0.2)
-ax.plot(date_time, x10, color='darkblue', lw=0.2)
+ax.set_title('UW, J1, J2, J3')
+ax.plot(date_time, x7, color='red', lw=0.2)       #UW
+ax.plot(date_time, x8, color='darkred', lw=0.2)   #J1
+ax.plot(date_time, x9, color='darkviolet', lw=0.2)#J2
+ax.plot(date_time, x10, color='darkblue', lw=0.2) #J3
 ax.plot(date_time, m7, color='red', lw=0.7)       #UW
 ax.plot(date_time, m8, color='darkred', lw=0.7)   #J1
 ax.plot(date_time, m9, color='darkviolet', lw=0.7)#J2
@@ -80,24 +103,29 @@ fig.autofmt_xdate()
 
 plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
 ax = fig.add_subplot(223)
-ax.set_title('ZB1+2, BD, WD, BG')
-ax.plot(date_time, x11, color='red', lw=0.5)
-ax.plot(date_time, x12, color='darkred', lw=0.5)
-ax.plot(date_time, x13, color='violet', lw=0.5)
-ax.plot(date_time, x14, color='darkblue', lw=0.5)
+ax.set_title('Z1, Z2, BD, WD, BG')
+ax.plot(date_time, x12, color='red', lw=0.5) #Z1
+ax.plot(date_time, x13, color='darkred', lw=0.5) #Z2
+ax.plot(date_time, x14, color='violet', lw=0.5) #BD
+ax.plot(date_time, x15, color='darkblue', lw=0.5) #WD
+ax.plot(date_time, x16, color='darkblue', lw=0.5) #BG
 ax.plot(date_time, m11, color='red', lw=0.7)        #Z1
 ax.plot(date_time, m12, color='darkred', lw=0.7)    #Z2
 ax.plot(date_time, m13, color='darkviolet', lw=0.7) #BD
-ax.plot(date_time, m14, color='darkblue', lw=0.7)   #WO
+ax.plot(date_time, m14, color='darkblue', lw=0.7)   #WD
 ax.plot(date_time, m15, color='darkblue', lw=0.7)   #BG
 plt.xlabel('time[h]')
 
 ax = fig.add_subplot(224)
-ax.set_title('DS, BI_U, EB, MD')
-ax.plot(date_time, x15, color='red', lw=0.5, label='1')
-ax.plot(date_time, x16, color='darkred', lw=0.5,label='2')
-ax.plot(date_time, x17, color='violet', lw=0.5, label='3')
-ax.plot(date_time, x18, color='darkblue', lw=0.5, label='4')
+ax.set_title('DS_P, BI_P, EB, MO')
+ax.plot(date_time, x20, color='red', lw=0.5, label='1') #DS_P+U
+ax.plot(date_time, x21, color='darkred', lw=0.5,label='2') #BI_P
+ax.plot(date_time, x23, color='violet', lw=0.5, label='3') #EB
+ax.plot(date_time, x27, color='darkblue', lw=0.5, label='4') #MO
+ax.plot(date_time, m19, color='red', lw=0.7)        #DS_P
+ax.plot(date_time, m21, color='darkred', lw=0.7)    #BI_P
+ax.plot(date_time, m24, color='darkviolet', lw=0.7) #EB
+#ax.plot(date_time, m28, color='darkblue', lw=0.7)   #MO
 
 fig.autofmt_xdate()
 plt.xlabel('time[h]')
