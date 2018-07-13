f2b8a00a4fe29f3d83f71a4527b9df3ea59a5765
wasserblum <heidelinde.trimmel@boku.ac.at>
2018-07-13 16:39:09 +0200

    WRF albedo

--- 4_URBANIA/displaySURFEX/Fig11_WRF_nc_albedo.py
+++ 4_URBANIA/displaySURFEX/Fig11_WRF_nc_albedo.py
@@ -12,22 +12,46 @@ file = '/media/lnx/98C4EEA4C4EE83BA/WRF-TEB-NC_Files/wrfout_d03_2015-07-13_12_00
 file2 = '/media/lnx/98C4EEA4C4EE83BA/WRF-TEB-NC_Files/HighAlbedo/wrfout_d03_2015-07-13_12_00_00'
 file3 = '/media/lnx/98C4EEA4C4EE83BA/WRF-TEB-NC_Files/HighAlbedo0_68Roof/wrfout_d03_2015-07-13_12_00_00'
 
+file4 = '/media/lnx/98C4EEA4C4EE83BA/WRF-TEB-NC_Files/2017_Teb/wrfout_d03_2017-07-27_00_00_00_joined.nc'
+file5 = '/media/lnx/98C4EEA4C4EE83BA/WRF-TEB-NC_Files/HighAlbedo0_3Roof_Canyon/wrfout_d03_2017-07-27_00_00_00'
+
 fh = Dataset(file, mode='r')
 fh2 = Dataset(file2, mode='r')
 fh3 = Dataset(file3, mode='r')
+fh4 = Dataset(file4, mode='r')
+fh5 = Dataset(file5, mode='r')
 
 '''time + 2dvariables'''
 lons = fh.variables['XLONG'][1]  #lon (147x)135x174
 lats = fh.variables['XLAT'][1]  #lat (147x)135x174
 tair = fh.variables['T2'] #147x135x174
-tair_units = fh.variables['T2'].units
 tair2 = fh2.variables['T2'] #147x135x174
 tair3 = fh3.variables['T2'] #147x135x174
 
-timeslices = 147
+swup1 = fh4.variables['SWUPB']
+swup2 = fh5.variables['SWUPB']
+swdown1 = fh4.variables['SWDOWN']
+swdown2 = fh5.variables['SWDOWN']
+
+tair_units = fh.variables['T2'].units
+swfx_units = fh.variables['SWUPB'].units
+pblh_units = fh.variables['PBLH'].units
+swdown_units = fh.variables['SWDOWN'].units
+
+#diff = (swup1[12]-swdown1[12]) - (swdown2[12]-swup2[12])
+#print len(diff[1])
+
+#print tair4[36]-tair5[36]
+#print tair5[36][40][30:40]
+
+#exit()
+
+'''>>>!!! CHECK CORRECT UNITS, TITLE, RANGE and FIGNAME'''
+
+timeslices = 72
 UTC=0
 for i in range(timeslices):
-  tair_diff = tair2[i]-tair[i]
+  rad_diff = (swup1[i]-swdown1[i]) - (swdown2[i]-swup2[i])
   m = Basemap(width=57943,height=44955,\
             rsphere=(6378137.00,6356752.3142),\
             projection='lcc',\
@@ -36,15 +60,18 @@ for i in range(timeslices):
             lon_0=16.37247,\
             resolution='l')
   xi, yi = m(lons, lats)
-  cs = m.pcolor(xi,yi,np.squeeze(tair_diff))
+  cs = m.pcolor(xi,yi,np.squeeze(rad_diff))
   cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
-  cbar.set_label(tair_units)
+  #cbar.set_label(tair_units)
+  cbar.set_label(swfx_units)
+  #cbar.set_label(pblh_units)
   hour = UTC % 24
   day = int(round(UTC/24, 0))
-  plt.title(r'2m Tair $\alpha_{r}:.15>.68$ day %s UTC %s' %(str(day), str(hour)))
-  UTC+=1
-  plt.clim(-2,2)
-  figname = outpath + str(i) + "WRFTEBalb.png"
+  UTC += 1
+  plt.title(r'$\delta$ sw rad balance $\alpha_{r}:0.30-0.15$ day %s UTC %s' %(str(day), str(hour)))
+  #plt.clim(-2,2)
+  #plt.clim(-2,80)
+  figname = outpath + "ALB_dSWradbal/" + str(i) + "WRFTEBalb2017_swbal.png"
   plt.savefig(figname)
 
   #plt.show()
@@ -73,6 +100,13 @@ for i in ZAMGnames:
   WRFdata3_heatdays = np.array(WRFdata3[12:-24])  # [8:176])
   WRFdata3_heatdays = WRFdata3_heatdays - 273.15
 
+  WRFdata4 = tair4[:, y, x]
+  WRFdata4_heatdays = np.array(WRFdata4)  # [8:176])
+  WRFdata4_heatdays = WRFdata4_heatdays - 273.15
+
+  WRFdata5 = tair5[:, y, x]
+  WRFdata5_heatdays = np.array(WRFdata5)  # [8:176])
+  WRFdata5_heatdays = WRFdata5_heatdays - 273.15
 
   try:
     '''
@@ -81,9 +115,11 @@ for i in ZAMGnames:
     fig = plt.figure()
     ax1 = fig.add_subplot(121)
     #ax1.plot(ZAMG_heatdays, color='black', label=u"Ground station")
-    ax1.plot(WRFdata_heatdays, color='red', label=r"WRF_TEB ($\alpha_{r}=0.15$)")  #31
-    ax1.plot(WRFdata2_heatdays, color='violet', label=r"WRF_TEB ($\alpha_{r}=0.30$)")  #31
-    ax1.plot(WRFdata3_heatdays, color='blue', label=r"WRF_TEB ($\alpha_{r}=0.68$)")  #31
+    #ax1.plot(WRFdata_heatdays, color='red', label=r"WRF_TEB ($\alpha_{r}=0.15$)")  #31
+    #ax1.plot(WRFdata2_heatdays, color='violet', label=r"WRF_TEB ($\alpha_{r}=0.30$)")  #31
+    #ax1.plot(WRFdata3_heatdays, color='blue', label=r"WRF_TEB ($\alpha_{r}=0.68$)")  #31
+    ax1.plot(WRFdata4_heatdays, color='red', label=r"WRF_TEB ($\alpha_{r}=0.15$)")  #31
+    ax1.plot(WRFdata5_heatdays, color='blue', label=r"WRF_TEB ($\alpha_{r}=0.30$)")  #31
     ax1.set_xlabel("hours[UTC]")
     ax1.set_ylabel(r"$T_{air_2m}$"u'[°C]', size="large")
     ax1.legend(loc='upper right')
@@ -100,7 +136,8 @@ for i in ZAMGnames:
     #ax2.set_ylabel(r"$T_{air_2m}$"u'[°C]', size="large")
     #ax2.legend(loc='upper left')
     #plt.suptitle(ZAMGnames[i] + ", 14 - 21 July 2015", size="large")#+"2m air temperature"))
-    plt.suptitle(ZAMGnames[i][0] + ", 14 - 18 July 2015", size="large")#+"2m air temperature"))
+    #plt.suptitle(ZAMGnames[i][0] + ", 14 - 18 July 2015", size="large")#+"2m air temperature"))
+    plt.suptitle(ZAMGnames[i][0] + ", 28 July - 4 Aug 2017", size="large")#+"2m air temperature"))
     #figname = outpath + i + "WRFTEBZAMG.png"
     #plt.savefig(figname)
     plt.show()
--- 4_URBANIA/displaySURFEX/Fig3_plot_SURFEX_nc_SURFATM.py
+++ 4_URBANIA/displaySURFEX/Fig3_plot_SURFEX_nc_SURFATM.py
@@ -6,29 +6,53 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.basemap import Basemap
 import csv
+
+'''This file plots 2D maps of SURFEX.nc files.  
+This is the cleaned version of the script, for all comments see Fig4_plotSURFFEXDIFF_nc1.py
+For hourly loop look at Fig5_plotSURFEX_nc1_hourly.py'''
+
+file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S12/SURF_ATM_DIAGNOSTICS.OUT.nc'
+file2 = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S13/SURF_ATM_DIAGNOSTICS.OUT.nc'
 #file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S12_FORC_WRF_333_STQ_2DURBPARAM_long/SURF_ATM_DIAGNOSTICS.OUT.nc'
 #file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S17_XUNIF/SURF_ATM_DIAGNOSTICS.OUT.nc'
-file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S15_XUNIF/SURF_ATM_DIAGNOSTICS.OUT.nc'
+#file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S15_XUNIF/SURF_ATM_DIAGNOSTICS.OUT.nc'
 #file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S15_XUNIF/SURF_ATM_DIAGNOSTICS.OUT.nc'
 #file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/DIFF_S4_S0_SURF_ATM_D.nc'
 #file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S0_FORC_WRF_333_STQ_long/SURF_ATM_DIAGNOSTICS.OUT.nc'
 #file = '/home/lnx/MODELS/SURFEX/2_source/SURFEX_TRUNK_4818/trunk/MY_RUN/KTEST/hapex/S3_LNX_WRF_333_nGD/SURF_ATM_DIAGNOSTICS.OUT.nc'
 #file = '/home/lnx/MODELS/SURFEX/3_input/met_forcing/FORCING_new.nc'
 fh = Dataset(file, mode='r')
+fh2 = Dataset(file2, mode='r')
+hour = 18
 
 lons = fh.variables['xx'][:]  #lon
 lats = fh.variables['yy'][:]  #lat
+
+RN = fh.variables['RN'][121]
+H = fh.variables['H'][121]
+LE = fh.variables['LE'][121]
+GFLUX = fh.variables['GFLUX'][121]
+
+RN2 = fh2.variables['RN'][121]
+H2 = fh2.variables['H'][121]
+LE2 = fh2.variables['LE'][121]
+GFLUX2 = fh2.variables['GFLUX'][121]
+
 tair0UTC = fh.variables['T2M'][55]
 tair6UTC = fh.variables['T2M'][61]
 tair12UTC = fh.variables['T2M'][67]
 tair18UTC = fh.variables['T2M'][73]
 wind10m =fh.variables['W10M'][73]
+
 #starts at 17.7.18hUTC [0], hourly ->
 # 18.7.0h [7],6h[13], 12h [19], 14h [21], 18h [42], 6h [54]
 # 20.7.  COLDEST: 0h[55], 5h[60], 6h[61], 12h [67], 14h[69], HOTTEST 15h[70], 18h [73]
+# 22.7.  0h[103], 6h[109], 12h [115], 18h [121]
 
+heatflux_units = fh.variables['RN'].units
 tair_units = fh.variables['T2M'].units
 wind10m_units = fh.variables['W10M'].units
+
 fh.close()
 
 '''CONVERSTION TO CELSIUS'''
@@ -46,7 +70,7 @@ tair6UTC_C = Kelvin_to_Celsius(tair6UTC)
 tair12UTC_C = Kelvin_to_Celsius(tair12UTC)
 tair18UTC_C = Kelvin_to_Celsius(tair18UTC)
 
-#CONVERSION TO LATLON
+'''CONVERSION TO LATLON'''
 lons2,lats2 =[],[]
 for i in lons:
     i2 = i*0.000014038 + 15.969 - (0.000014038*333)
@@ -55,9 +79,6 @@ for i in lats:
     i2 = i*0.000009355 + 48.0322418 - (0.000009355*333)
     lats2.append(i2)
 
-#print lons2[0], lons2[-1], lats2[0],lats2[-1],
-#lon_0 = lons.mean()
-#lat_0 = lats.mean()
 m = Basemap(width=57943,height=44955,\
             rsphere=(6378137.00,6356752.3142),\
             projection='lcc',\
@@ -66,37 +87,83 @@ m = Basemap(width=57943,height=44955,\
             lon_0=16.37247,\
             resolution='l')
 
-# Add Grid Lines
-m.drawparallels(np.arange(44.0, 50., 0.1), labels=[1,0,0,0], fontsize=10, linewidth=0.)
-m.drawmeridians(np.arange(14.0, 17., 0.2), labels=[0,0,0,1], fontsize=10, linewidth=0.)
-
-# Add Coastlines, States, and Country Boundaries
-m.drawcoastlines()
-m.drawstates()
-m.drawcountries()
-
-# Because our lon and lat variables are 1D,
-# use meshgrid to create 2D arrays
-# Not necessary if coordinates are already in 2D arrays.
 lon, lat = np.meshgrid(lons2, lats2)
 xi, yi = m(lon, lat)
-#lons = 174: 333 -57942, lats = 135:333 -44955, tair = 36[[][] ]
-#lon, lat, xi,yi = 135x174
-#print "lons=", lons, "lats=", lats, "tair=", tair,"lon=", lon, "lat=", lat, "xi=", xi, "yi=", yi
-#print len(lons), len(lats), len(tair[0]), len(lon[0]), len(lat[0]), len(xi[0]),len(yi[0]),
-# Plot Data
 
-#clevs = [0,1,2.5,5,7.5,10,15,20,30,40,50,70,100,150,200,250,300,400,500,600,750]
-#cs = m.contourf(x,y,data,clevs,cmap=cm.s3pcpn)
+'''Heat fluxes'''
+
+dRN = RN2 -RN
+dH = H2-H
+dLE = LE2 - LE
+dGFLUX = GFLUX2 - GFLUX
+
+cs = m.pcolor(xi,yi,np.squeeze(RN))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Radiation Balance STQ 2015-07-22 %s UTC' %(hour))
+#plt.clim(21,35)
+plt.show()
+
+cs = m.pcolor(xi,yi,np.squeeze(dRN))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Radiation Balance S1-STQ 2015-07-22 %s UTC' %(hour))
+plt.clim(-20,20)
+plt.show()
+
+
+cs = m.pcolor(xi,yi,np.squeeze(GFLUX))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Ground Heat Flux STQ 2015-07-22 %s UTC' %(hour))
+#plt.clim(21,35)
+plt.show()
+
+cs = m.pcolor(xi,yi,np.squeeze(dGFLUX))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Ground Heat Flux S1-STQ 2015-07-22 %s UTC' %(hour))
+#plt.clim(21,35)
+plt.show()
+
+
+cs = m.pcolor(xi,yi,np.squeeze(H))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Sensible Heat Flux STQ 2015-07-22 %s UTC' %(hour))
+#plt.clim(21,35)
+plt.show()
+
+cs = m.pcolor(xi,yi,np.squeeze(dH))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Sensible Heat Flux S1-STQ 2015-07-22 %s UTC' %(hour))
+#plt.clim(21,35)
+plt.show()
+
+
+cs = m.pcolor(xi,yi,np.squeeze(LE))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Latent Heat Flux STQ 2015-07-22 %s UTC' %(hour))
+#plt.clim(21,35)
+plt.show()
+
+cs = m.pcolor(xi,yi,np.squeeze(dLE))
+cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
+cbar.set_label(heatflux_units)
+plt.title('Latent Heat Flux S1-STQ 2015-07-22 %s UTC' %(hour))
+#plt.clim(21,35)
+plt.show()
+
+exit()
+
+
+'''Air temperature'''
 
 #cs = m.pcolor(xi,yi,np.squeeze(tairC))
 cs = m.pcolor(xi,yi,np.squeeze(tair0UTC_C))
-
-
-# Add Colorbar
 cbar = m.colorbar(cs, location='bottom', pad="10%", extend="both")
-
-
 #cbar.set_label(tair_units)
 cbar.set_label(tairC_units)
 #cbar.set_label(wind10m_units)
