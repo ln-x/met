988fc404cd4639d81a11b1f69c1d69b939ad9a03
heidit <heidelinde.trimmel>
2021-01-14 14:16:38 +0100

    2021_01-07

--- 5_UOZONE/4_Auswertung_HCHO.py
+++ 5_UOZONE/4_Auswertung_HCHO.py
@@ -146,7 +146,8 @@ Apolis_GS = "/windata/GOOGLEDrive/DATA/obs_point/met/Apolis/apolis1990.2019_o3_s
 ## calculate 90% of max globrad -> threshold
 #thres.glob <- (1.25e-12*x^5+ 7.38E-09*x^4 - 5.365E-06*x^3 + 0.000926*x^2 - 0.00036*x + 1.08)*0.9
 
-"""FIG1"""
+"""FIG1 monthly mean diurnal cycles"""
+"""
 fig, ax = plt.subplots(1, figsize=(12,6))
 
 #dataframe['Month'] = dataframe.index.map(lambda x: x.strftime("%m"))
@@ -158,39 +159,82 @@ pd.plotting.register_matplotlib_converters()
 
 colors = ["k","gray","silver","greenyellow","chartreuse","green","yellow","orange","red","sandybrown","goldenrod","saddlebrown", "grey"]
 
+AxisA_monthly_mean = []
 for month in AxisA_hr_mean['Month'].unique():
     df = AxisA_hr_mean.loc[AxisA_hr_mean['Month'] == month]
     df = df.groupby('Time').describe() #.mean()
     #print(df['HCHO']['mean'])       #12x24h ok!
     #print(df.index) #12x24h ok!
     ax.plot(df['HCHO']['mean'], linewidth=2.0, color=colors[month], label = month)
-
+    AxisA_monthly_mean.append(df['HCHO']['mean'])
 ax.legend()
+
+#print(AxisA_monthly_mean)
+
 
 ticks = ax.get_xticks()
 ax.set_xticks(np.linspace(ticks[0], d.date2num(d.num2date(ticks[-1]) + dt.timedelta(hours=1)), 5))
 ax.set_xticks(np.linspace(ticks[0], d.date2num(d.num2date(ticks[-1]) + dt.timedelta(hours=1)), 25), minor=True)
-plt.show()
+#plt.show()
+"""
+"""FIG 1 END"""
+
+
+"""FIG 2 scatter plot """
+"""
+print(len(AxisD_hr_mean[:-1004]), len(timeaxis_NO2[7255::2]))  #JD 31.Mai: 151*(24*2 half hours)=7248
+print(AxisD_hr_mean[:-1004])
+print(timeaxis_NO2[7255::2])
+try:
+    fig = plt.figure()
+    #plt.scatter(AxisA_hr_mean[:-1004], NO2_HMW["STEF"][7255::2], color='navy', label="hcho_obs_A vs. NO2_STEF")
+    plt.scatter(AxisD_hr_mean[:-1003], NO2_HMW["STEF"][7255::2], color='navy', label="hcho_obs_D vs. NO2_STEF")
+    plt.legend(loc='upper left')
+    plt.show()
+except:
+    print("Oops!", sys.exc_info()[0], "occured.")
+    pass
+"""
+"""FIG 2 END"""
+
+NO2_HMW = NO2_HMW.set_index(pd.to_datetime(NO2_HMW['timestamp.MEZ']))
+NO2_HMW_drop = NO2_HMW.drop(columns=['timestamp.MEZ'])
+NO2_SMW_STEF = NO2_HMW_drop["STEF"][1::2]
+print(NO2_SMW_STEF)
+NO2_SMW_STEF['Month'] = NO2_SMW_STEF.index.month
+NO2_SMW_STEF['Time'] = NO2_SMW_STEF.index.time
+pd.plotting.register_matplotlib_converters()
+#colors = ["k","gray","silver","greenyellow","chartreuse","green","yellow","orange","red","sandybrown","goldenrod","saddlebrown", "grey"]
+NO2STEF_monthly_mean = []
+for month in NO2_SMW_STEF['Month'].unique():
+    df = NO2_SMW_STEF.loc[NO2_SMW_STEF['Month'] == month]
+    df = df.groupby('Time').describe() #.mean()
+    print(df)
+    print(df.index)
+    #ax.plot(df['HCHO']['mean'], linewidth=2.0, color=colors[month], label = month)
+    #NO2STEF_monthly_mean.append(df['HCHO']['mean'])
 
 exit()
 
-"""FIG 1 END"""
-
+"""FIG 3 simple timeseries """
 try:
     fig = plt.figure()
     ax1 = plt.gca()
     ax2 = ax1.twinx()
-    ax1.plot(AxisA_hr_mean, color='navy', label="hcho_obs_A")
-    ax1.plot(AxisB_hr_mean, color='royalblue', label="hcho_obs_B")
-    ax1.plot(AxisC_hr_mean, color='cyan', label="hcho_obs_C")
-    ax1.plot(AxisD_hr_mean, color='yellow', label="hcho_obs_D")
-    ax1.plot(AxisF_hr_mean, color='red', label="hcho_obs_F")
-    ax1.plot(AxisG_hr_mean, color='brown', label="hcho_obs_G")
+    #ax1.plot(AxisA_hr_mean, color='navy', label="hcho_obs_A")
+    #ax1.plot(AxisB_hr_mean, color='royalblue', label="hcho_obs_B")
+    #ax1.plot(AxisC_hr_mean, color='cyan', label="hcho_obs_C")
+    #ax1.plot(AxisD_hr_mean, color='yellow', label="hcho_obs_D")
+    #ax1.plot(AxisF_hr_mean, color='red', label="hcho_obs_F")
+    #ax1.plot(AxisG_hr_mean, color='brown', label="hcho_obs_G")
 
     #ax1.plot(NO2_HMW["STEF"][1::2], color='blue', label="no2_obs")
     #ax1.plot(timeaxis_NO2[1::2], NO2_HMW["STEF"][1::2], color='blue', label="no2_obs")
     #ax1.plot(timeaxis_NO[1::2], NO_HMW["STEF"][1::2], color='pink', label="no_obs")
     #ax1.plot(timeaxis_O3[1::2], O3_HMW["HMW.O3.STEF.09.ug.m3.."][1::2], color='violet', label="o3_obs")
+
+    ax1.plot(AxisA_hr_mean[:-1004], timeaxis_NO2[7255::2], color='navy', label="hcho_obs_A vs. NO2_STEF")
+
     ax1.legend(loc='upper left')
     ax1.set_ylabel("ug m3", size="medium")
 
@@ -211,3 +255,4 @@ try:
 except:
     print("Oops!", sys.exc_info()[0], "occured.")
     pass
+
